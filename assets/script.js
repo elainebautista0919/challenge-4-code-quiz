38dbb09750266425b777dd12f4a6078b7cb9d42c
Elaine Bautista <elainebautista@Elaines-MacBook-Pro.local>
2022-10-29 10:21:17 -0400

    Added questions and answers in JS

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// Var with array and objecct for questions
+var questions = [
+    {
+        title: "What does HTML stand for?",
+        choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "None of the above"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        title: "What does CSS stand for?",
+        choices: ["Cascading Style Sheets", "Coloured Special Sheets", "Color and Style Sheets", "None of the above"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        title: "How do you insert a line break in HTML?",
+        choices: ["<br>", "<bk>", "<space>", "<p>"],
+        answer: "<br>"
+    },
+    {
+        title: "How can we change the text color of an element?",
+        choices: ["background-color", "color", "text-align", "padding"],
+        answer: "color"
+    },
+    {
+        title: "Inside which HTML element do we put the Javascript?",
+        choices: ["<script>", "<js>", "<javascript>", "<link>"],
+        answer: "<script>"
+    },
+];
+
